990ad274fa8a8e3dbe8e32d0a88e8da16bf33405
guilherme <guilhermesilvas2004@gmail.com>
2023-10-01 17:44:40 -0300

    Condicionais e Variaveis

---
+++ Basic/condicionais.py
@@ -0,0 +1,18 @@
+
+
+
+placa = int(input("Digite o primeiro número da sua plca do carro: "))
+
+
+if (placa == 1 or placa == 2):
+    print("Segunda-feira")
+elif (placa == 3 or placa == 4):
+    print("Terça-feira")
+elif (placa == 5 or placa == 6):
+    print("Quarta-feira")
+elif (placa == 7 or placa == 8):
+    print("Quinta-feira")
+elif (placa == 0 or placa == 9):
+    print("Sexta-feira")
+else:
+    print("Houve um erro de digitação, por favor, digite novamente!")
---
+++ Basic/variaveis.py
@@ -0,0 +1,23 @@
+# > VARIÀVEIS
+
+# 1. Variáveis
+
+nome = 'Brazilian Code' 
+idade = 24
+altura = 1.79
+estudando = True #verdadeiro 
+
+'''
+1. int : número inteiros, ou seja, números sem parte decimal: 0, 5, -1, 1000
+2. Float: números reais, ou seja, números com parte decimal : 1.0, -2.7, 3.14
+3. str: cadeias de caracteres, ou seja, dados textuais (string)
+4. bool: valores lógicos (booleanos): True ou False
+'''
+print(type(nome))
+print(type(idade))
+print(type(altura))
+print(type(estudando))
+
+
+nome_p =  input('Qual é o seu nome? ')
+print('Seu nome é: ', nome_p)
